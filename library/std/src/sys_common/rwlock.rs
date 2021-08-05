0b1b075bfe585ff053142edd2164b4e1f551302f
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2021-08-05 19:14:38 +0200

    [SOL] Adjust BPF customization after upgrading to rust 1.54.0

--- library/std/src/sys_common/rwlock.rs
+++ library/std/src/sys_common/rwlock.rs
@@ -6,8 +6,10 @@ use crate::sys::locks as imp;
 ///
 /// This rwlock has a const constructor ([`StaticRwLock::new`]), does not
 /// implement `Drop` to cleanup resources.
+#[cfg(not(target_arch = "bpf"))]
 pub struct StaticRwLock(imp::RwLock);
 
+#[cfg(not(target_arch = "bpf"))]
 impl StaticRwLock {
     /// Creates a new rwlock for use.
     #[inline]
@@ -36,9 +38,11 @@ impl StaticRwLock {
     }
 }
 
+#[cfg(not(target_arch = "bpf"))]
 #[must_use]
 pub struct StaticRwLockReadGuard(&'static imp::RwLock);
 
+#[cfg(not(target_arch = "bpf"))]
 impl Drop for StaticRwLockReadGuard {
     #[inline]
     fn drop(&mut self) {
@@ -48,9 +52,11 @@ impl Drop for StaticRwLockReadGuard {
     }
 }
 
+#[cfg(not(target_arch = "bpf"))]
 #[must_use]
 pub struct StaticRwLockWriteGuard(&'static imp::RwLock);
 
+#[cfg(not(target_arch = "bpf"))]
 impl Drop for StaticRwLockWriteGuard {
     #[inline]
     fn drop(&mut self) {
