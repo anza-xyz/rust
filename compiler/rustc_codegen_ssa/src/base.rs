62d4bfff0e9759f898ab117f3a901eacdfe7c113
Alessandro Decina <alessandro.d@gmail.com>
2021-11-26 11:05:15 +1100

    [SOL] rework test harness codegen a bit
    This change reworks the way we generate the test harness, it removes  most
    bpf-only changes,  implements a custom start entry for the bpf arch, and tweaks
    the link stage to set the correct entry point depending on whether we're
    compiling a library or tests.
    
    With this change, artifacts produced by rustc --test can be loaded in rbpf like
    any other library.

--- compiler/rustc_codegen_ssa/src/base.rs
+++ compiler/rustc_codegen_ssa/src/base.rs
@@ -530,9 +530,7 @@ pub fn maybe_create_entry_wrapper<'a, 'tcx, Bx: BuilderMethods<'a, 'tcx>>(
         let llbb = Bx::append_block(cx, llfn, "top");
         let mut bx = Bx::build(cx, llbb);
 
-        if !is_bpf {
-            bx.insert_reference_to_gdb_debug_scripts_section_global();
-        }
+        bx.insert_reference_to_gdb_debug_scripts_section_global();
 
         let isize_ty = cx.type_isize();
         let ptr_ty = cx.type_ptr();
