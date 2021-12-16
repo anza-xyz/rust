b353d198912b4cd6e9bd7a9150b2cd7bc12fe532
Alessandro Decina <alessandro.d@gmail.com>
2021-12-16 12:59:44 +1100

    [SOL] make all core tests (but atomics) pass
    This makes all the core tests pass.
    
    - Removes the use of thread_local!()s. The API can't be implemented without
      adding rbpf shims, which seems pointless.
    - Hardcodes ThreadId::new() to return 1, disabling upstream code that creates
      static muts.
    - Re-enables UpperExp / LowerExp impls - only float code stays disabled
    - Reduces stack size in some tests.
    - Hardcodes a static seed in tests that use the rand crate.

--- library/core/tests/lib.rs
+++ library/core/tests/lib.rs
@@ -139,15 +139,12 @@ mod clone;
 mod cmp;
 mod const_ptr;
 mod convert;
-#[cfg(not(target_arch = "bpf"))]
 mod fmt;
 mod future;
-#[cfg(not(target_arch = "bpf"))]
 mod hash;
 mod intrinsics;
 mod io;
 mod iter;
-#[cfg(not(target_arch = "bpf"))]
 mod lazy;
 #[cfg(test)]
 mod macros;
@@ -155,7 +152,6 @@ mod manually_drop;
 mod mem;
 mod net;
 mod nonzero;
-#[cfg(not(target_arch = "bpf"))]
 mod num;
 mod ops;
 mod option;
@@ -163,11 +159,9 @@ mod panic;
 mod pattern;
 mod pin;
 mod pin_macro;
-#[cfg(not(target_arch = "bpf"))]
 mod ptr;
 mod result;
 mod simd;
-#[cfg(not(target_arch = "bpf"))]
 mod slice;
 mod str;
 mod str_lossy;
--- library/std/src/thread/mod.rs
+++ library/std/src/thread/mod.rs
@@ -1277,7 +1277,7 @@ impl ThreadId {
                         Err(id) => last = id,
                     }
                 }
-            } else {
+            } else if #[cfg(not(any(target_arch = "bpf", target_arch = "sbf")))] {
                 use crate::sync::{Mutex, PoisonError};
 
                 static COUNTER: Mutex<u64> = Mutex::new(0);
@@ -1293,6 +1293,11 @@ impl ThreadId {
                 *counter = id;
                 drop(counter);
                 ThreadId(NonZero::new(id).unwrap())
+            } else {
+                // threads are not supported in sbf, so this isn't actually used
+                // anywhere. This branch of the if is only to avoid creating static
+                // mutable data.
+                ThreadId(NonZero::new(1).unwrap())
             }
         }
     }
