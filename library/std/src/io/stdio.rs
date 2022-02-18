af07fd055609fe9df05efc9fea68b3145772d94a
Brian Anderson <andersrb@gmail.com>
2022-02-18 09:51:20 -0700

    [SOL] Don't create invalid string slices in stdout/stderr on Solana

--- library/std/src/io/stdio.rs
+++ library/std/src/io/stdio.rs
@@ -907,9 +907,7 @@ impl Write for Stdout {
 #[cfg(any(target_arch = "bpf", target_arch = "sbf"))]
 impl Write for Stdout {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        unsafe {
-            crate::sys::sol_log(core::str::from_utf8_unchecked(buf));
-        }
+        crate::sys::sol_log(buf);
         Ok(buf.len())
     }
     fn write_vectored(&mut self, _bufs: &[IoSlice<'_>]) -> io::Result<usize> {
@@ -923,9 +921,7 @@ impl Write for Stdout {
         Ok(())
     }
     fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
-        unsafe {
-            crate::sys::sol_log(core::str::from_utf8_unchecked(buf));
-        }
+        crate::sys::sol_log(buf);
         Ok(())
     }
     fn write_all_vectored(&mut self, _bufs: &mut [IoSlice<'_>]) -> io::Result<()> {
@@ -1254,9 +1250,7 @@ impl Write for Stderr {
 #[cfg(any(target_arch = "bpf", target_arch = "sbf"))]
 impl Write for Stderr {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        unsafe {
-            crate::sys::sol_log(core::str::from_utf8_unchecked(buf));
-        }
+        crate::sys::sol_log(buf);
         Ok(buf.len())
     }
     fn write_vectored(&mut self, _bufs: &[IoSlice<'_>]) -> io::Result<usize> {
@@ -1270,9 +1264,7 @@ impl Write for Stderr {
         Ok(())
     }
     fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
-        unsafe {
-            crate::sys::sol_log(core::str::from_utf8_unchecked(buf));
-        }
+        crate::sys::sol_log(buf);
         Ok(())
     }
     fn write_all_vectored(&mut self, _bufs: &mut [IoSlice<'_>]) -> io::Result<()> {
