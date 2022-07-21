9e501d6fe2633e815b710159bc1329fcd1cba0ab
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2022-07-20 17:31:43 -0700

    [SOL] Adjust BPF customization after upgrading to rust 1.62.0

--- library/std/src/panicking.rs
+++ library/std/src/panicking.rs
@@ -99,7 +99,7 @@ enum Hook {
     Custom(Box<dyn Fn(&PanicHookInfo<'_>) + 'static + Sync + Send>),
 }
 
-#[cfg(not(target_os = "solana"))]
+#[cfg(not(any(target_arch = "bpf", target_arch = "sbf")))]
 impl Hook {
     #[inline]
     fn into_box(self) -> Box<dyn Fn(&PanicHookInfo<'_>) + 'static + Sync + Send> {
@@ -204,7 +204,7 @@ pub fn set_hook(_hook: Box<dyn Fn(&PanicInfo<'_>) + 'static + Sync + Send>) {
 /// panic!("Normal panic");
 /// ```
 #[must_use]
-#[cfg(all(not(target_arch = "bpf"), not(target_arch = "sbf")))]
+#[cfg(not(any(target_arch = "bpf", target_arch = "sbf")))]
 #[stable(feature = "panic_hooks", since = "1.10.0")]
 pub fn take_hook() -> Box<dyn Fn(&PanicHookInfo<'_>) + 'static + Sync + Send> {
     if thread::panicking() {
@@ -212,6 +212,13 @@ pub fn take_hook() -> Box<dyn Fn(&PanicHookInfo<'_>) + 'static + Sync + Send> {
     }
 
     HOOK.replace(Hook::Default).into_box()
+}
+
+/// Dummy version for satisfying library/test dependencies for BPF target
+#[cfg(any(target_arch = "bpf", target_arch = "sbf"))]
+#[stable(feature = "panic_hooks", since = "1.10.0")]
+pub fn take_hook() -> Box<dyn Fn(&PanicInfo<'_>) + 'static + Sync + Send> {
+    Box::new(default_hook)
 }
 
 /// Atomic combination of [`take_hook`] and [`set_hook`]. Use this to replace the panic handler with
@@ -245,6 +252,7 @@ pub fn take_hook() -> Box<dyn Fn(&PanicHookInfo<'_>) + 'static + Sync + Send> {
 ///
 /// panic!("Custom and then normal");
 /// ```
+#[cfg(not(any(target_arch = "bpf", target_arch = "sbf")))]
 #[unstable(feature = "panic_update_hook", issue = "92649")]
 pub fn update_hook<F>(hook_fn: F)
 where
@@ -262,16 +270,21 @@ where
     *hook = Hook::Custom(Box::new(move |info| hook_fn(&prev, info)));
 }
 
-/// Dummy version for satisfying library/test dependencies for BPF target
-#[cfg(target_os = "solana")]
-#[stable(feature = "panic_hooks", since = "1.10.0")]
-pub fn take_hook() -> Box<dyn Fn(&PanicInfo<'_>) + 'static + Sync + Send> {
-    Box::new(default_hook)
+/// Dummy version for satisfying library/test dependencies for SBF target
+#[cfg(any(target_arch = "bpf", target_arch = "sbf"))]
+#[unstable(feature = "panic_update_hook", issue = "92649")]
+pub fn update_hook<F>(_hook_fn: F)
+where
+    F: Fn(&(dyn Fn(&PanicInfo<'_>) + Send + Sync + 'static), &PanicInfo<'_>)
+        + Sync
+        + Send
+        + 'static,
+{
 }
 
 /// The default panic handler.
 #[optimize(size)]
-#[cfg(all(not(target_arch = "bpf"), not(target_arch = "sbf")))]
+#[cfg(not(any(target_arch = "bpf", target_arch = "sbf")))]
 fn default_hook(info: &PanicHookInfo<'_>) {
     // If this is a double panic, make sure that we print a backtrace
     // for this panic. Otherwise only print it if logging is enabled.
@@ -998,6 +1011,7 @@ pub fn begin_panic<M: Any + Send>(_msg: M) -> ! {
     let info = PanicInfo::internal_constructor(
         None,
         Location::caller(),
+        false,
     );
     crate::sys::panic(&info);
 }
@@ -1015,6 +1029,7 @@ pub fn begin_panic_fmt(msg: &fmt::Arguments<'_>) -> ! {
     let info = PanicInfo::internal_constructor(
         Some(msg),
         Location::caller(),
+        false,
     );
     crate::sys::panic(&info);
 }
