b083ce43c53c7f7dbcd85dfc82a3341d0cded37e
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2023-08-16 16:28:48 -0400

    [SOL] Set default visibility of symbols in SBF binaries to hidden
    Make a solana specific version of __rdl_oom handler that doesn't use a
    static variable.  This variable can't be supported is Solana execution
    environment.

--- library/alloc/src/alloc.rs
+++ library/alloc/src/alloc.rs
@@ -549,10 +549,19 @@ pub mod __alloc_error_handler {
     // called via generated `__rust_alloc_error_handler` if there is no
     // `#[alloc_error_handler]`.
     #[rustc_std_internal_symbol]
+    #[cfg(not(target_family = "solana"))]
     pub unsafe fn __rdl_alloc_error_handler(size: usize, _align: usize) -> ! {
         core::panicking::panic_nounwind_fmt(
             format_args!("memory allocation of {size} bytes failed"),
             /* force_no_backtrace */ false,
         )
+    }
+
+    #[rustc_std_internal_symbol]
+    #[cfg(target_family = "solana")]
+    pub unsafe fn __rdl_oom(size: usize, _align: usize) -> ! {
+        core::panicking::panic_nounwind_fmt(format_args!(
+            "memory allocation of {size} bytes failed"
+        ))
     }
 }
