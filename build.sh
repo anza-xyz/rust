7bcf784a6d958e85eb01fde39272ee010e520a07
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-12-22 10:45:03 -0500

    Bugfix: allow arm64 as output of uname -m

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ unameOut="$(uname -s)-$(uname -m)"
 case "${unameOut}" in
     Linux-x86_64*)  HOST_TRIPLE=x86_64-unknown-linux-gnu;;
     Linux-aarch64*) HOST_TRIPLE=aarch64-unknown-linux-gnu;;
+    Linux-arm64*)   HOST_TRIPLE=aarch64-unknown-linux-gnu;;
     Darwin-x86_64*) HOST_TRIPLE=x86_64-apple-darwin;;
     Darwin-arm64*)  HOST_TRIPLE=aarch64-apple-darwin;;
     MINGW*)         HOST_TRIPLE=x86_64-pc-windows-msvc;;
